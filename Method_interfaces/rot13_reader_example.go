0fa35e076cc86025521cd587a8e61d9efda94526
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2020-05-18 14:56:54 +0900

    Update rot13_reader_example.go

--- Method_interfaces/rot13_reader_example.go
+++ Method_interfaces/rot13_reader_example.go
@@ -4,32 +4,37 @@ import (
     "io"
     "os"
     "strings"
-    "fmt"
 )
 
 type rot13Reader struct {
     r io.Reader
 }
 
-func changeRot13(char byte)(cng byte){
-    return
+func rot13byte(sb byte) byte {
+    s := rune(sb)
+    if s >= 'a' && s <= 'm' || s >= 'A' && s <= 'M' {
+        sb += 13
+    }
+    if s >= 'n' && s <= 'z' || s >= 'N' && s <= 'Z' {
+        sb -= 13
 }
 
-
-func (rot *rot13Reader)Read(data []byte) (len int, err error) {
-    len, err = rot.r.Read(data)
-    fmt.Println(data)
-    // for i, char := range data{
-    //     //data[i] = changeRot13(data[i])
-    //     fmt.Println(data[i], char)
-    // }
-    return
+    return sb
 }
 
+func (rot13 rot13Reader) Read(data []byte) (len int, err error) {
+    len, err = rot13.r.Read(data)
+    for i := 0; i < len; i++ {
+        data[i] = rot13byte(data[i])
+    }
+
+    return
+}
 
 func main() {
     s := strings.NewReader(
-        "Lbh penpxrq gur pbqr!")
+    "Lbh penpxrq gur pbqr!")
     r := rot13Reader{s}
     io.Copy(os.Stdout, &r)
 }
+
